a4513ca3b912be215c3e737cb8dc2c8b8ac52656
Sourabh Sogani <sourabh.sogani@roche.com>
2026-03-23 17:06:39 +0530

    Refactor environment configuration and add redirects for deployment

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  baseUrl = 'http://localhost:3000';
+baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://fullstack-project-k353.onrender.com'
+};
